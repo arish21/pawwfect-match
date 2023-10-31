4ce915ebef70d6f38afc3e1cae3ec398cf0486d7
arish21 <arishdakhani99@gmail.com>
2023-10-31 15:00:56 -0400

    Change to app.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   );
   return (
     <NotificationProvider>
-      <Router basename="/">
+      <Router basename="/pawwfect-match">
         <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         <Routes>
           <Route exact path="/" element={<Home />} />
